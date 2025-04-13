2daf90472176edf413753a447bdb039f3fda499a
Tyler McBride <tmcbride2@my.monroeccc.edu>
2025-04-12 23:27:48 -0400

    GUI: Forms
    -Changed "Form1" to "Welcome" and rough drafted the GUI
    -Added all base forms to the file (all are empty still)

---
+++ ConnectFour_Group7/Welcome.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace ConnectFour_Group7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_welcome_singlePlayer = new System.Windows.Forms.Button();
+            this.lbl_welcome_title = new System.Windows.Forms.Label();
+            this.btn_welcome_versus = new System.Windows.Forms.Button();
+            this.btn_welcome_statistics = new System.Windows.Forms.Button();
+            this.btn_welcome_exit = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.panel1.Controls.Add(this.lbl_welcome_title);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(473, 52);
+            this.panel1.TabIndex = 0;
+            // 
+            // btn_welcome_singlePlayer
+            // 
+            this.btn_welcome_singlePlayer.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_welcome_singlePlayer.Location = new System.Drawing.Point(138, 129);
+            this.btn_welcome_singlePlayer.Name = "btn_welcome_singlePlayer";
+            this.btn_welcome_singlePlayer.Size = new System.Drawing.Size(174, 41);
+            this.btn_welcome_singlePlayer.TabIndex = 1;
+            this.btn_welcome_singlePlayer.Text = "Single Player";
+            this.btn_welcome_singlePlayer.UseVisualStyleBackColor = true;
+            // 
+            // lbl_welcome_title
+            // 
+            this.lbl_welcome_title.AutoSize = true;
+            this.lbl_welcome_title.Font = new System.Drawing.Font("Comic Sans MS", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_welcome_title.Location = new System.Drawing.Point(3, 0);
+            this.lbl_welcome_title.Name = "lbl_welcome_title";
+            this.lbl_welcome_title.Size = new System.Drawing.Size(221, 50);
+            this.lbl_welcome_title.TabIndex = 2;
+            this.lbl_welcome_title.Text = "Connect Fo\'";
+            // 
+            // btn_welcome_versus
+            // 
+            this.btn_welcome_versus.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_welcome_versus.Location = new System.Drawing.Point(138, 190);
+            this.btn_welcome_versus.Name = "btn_welcome_versus";
+            this.btn_welcome_versus.Size = new System.Drawing.Size(174, 41);
+            this.btn_welcome_versus.TabIndex = 2;
+            this.btn_welcome_versus.Text = "Vs.";
+            this.btn_welcome_versus.UseVisualStyleBackColor = true;
+            // 
+            // btn_welcome_statistics
+            // 
+            this.btn_welcome_statistics.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_welcome_statistics.Location = new System.Drawing.Point(138, 255);
+            this.btn_welcome_statistics.Name = "btn_welcome_statistics";
+            this.btn_welcome_statistics.Size = new System.Drawing.Size(174, 41);
+            this.btn_welcome_statistics.TabIndex = 3;
+            this.btn_welcome_statistics.Text = "Stats";
+            this.btn_welcome_statistics.UseVisualStyleBackColor = true;
+            // 
+            // btn_welcome_exit
+            // 
+            this.btn_welcome_exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_welcome_exit.Location = new System.Drawing.Point(138, 595);
+            this.btn_welcome_exit.Name = "btn_welcome_exit";
+            this.btn_welcome_exit.Size = new System.Drawing.Size(174, 41);
+            this.btn_welcome_exit.TabIndex = 4;
+            this.btn_welcome_exit.Text = "Exit";
+            this.btn_welcome_exit.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(473, 672);
+            this.Controls.Add(this.btn_welcome_exit);
+            this.Controls.Add(this.btn_welcome_statistics);
+            this.Controls.Add(this.btn_welcome_versus);
+            this.Controls.Add(this.btn_welcome_singlePlayer);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btn_welcome_singlePlayer;
+        private System.Windows.Forms.Label lbl_welcome_title;
+        private System.Windows.Forms.Button btn_welcome_versus;
+        private System.Windows.Forms.Button btn_welcome_statistics;
+        private System.Windows.Forms.Button btn_welcome_exit;
+    }
+}
+
