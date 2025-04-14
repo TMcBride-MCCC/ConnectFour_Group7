825063f0d9d554ef3f09d96ea749fa32e60f6d58
Tyler McBride <tmcbride2@my.monroeccc.edu>
2025-04-13 23:16:27 -0400

    SinglePlayer.cs
    Worked on the GUI for the SinglePlayer form. I can't find a transparent white circle to add. Will look again tomorrow and go from there.

--- ConnectFour_Group7/SinglePlayer.Designer.cs
+++ ConnectFour_Group7/SinglePlayer.Designer.cs
@@ -31,13 +31,16 @@
             this.lbl_singlePlayer_title = new System.Windows.Forms.Label();
             this.btn_singlePlayer_exit = new System.Windows.Forms.Button();
             this.btn_singlePlayer_returnToMain = new System.Windows.Forms.Button();
+            this.panel_singlePlayer_top = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel_singlePlayer_top.SuspendLayout();
             this.SuspendLayout();
             // 
             // lbl_singlePlayer_title
             // 
             this.lbl_singlePlayer_title.AutoSize = true;
             this.lbl_singlePlayer_title.Font = new System.Drawing.Font("Comic Sans MS", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_singlePlayer_title.Location = new System.Drawing.Point(272, 9);
+            this.lbl_singlePlayer_title.Location = new System.Drawing.Point(3, 0);
             this.lbl_singlePlayer_title.Name = "lbl_singlePlayer_title";
             this.lbl_singlePlayer_title.Size = new System.Drawing.Size(242, 50);
             this.lbl_singlePlayer_title.TabIndex = 3;
@@ -46,7 +49,7 @@
             // btn_singlePlayer_exit
             // 
             this.btn_singlePlayer_exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_singlePlayer_exit.Location = new System.Drawing.Point(614, 397);
+            this.btn_singlePlayer_exit.Location = new System.Drawing.Point(614, 534);
             this.btn_singlePlayer_exit.Name = "btn_singlePlayer_exit";
             this.btn_singlePlayer_exit.Size = new System.Drawing.Size(174, 41);
             this.btn_singlePlayer_exit.TabIndex = 5;
@@ -57,7 +60,7 @@
             // btn_singlePlayer_returnToMain
             // 
             this.btn_singlePlayer_returnToMain.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_singlePlayer_returnToMain.Location = new System.Drawing.Point(614, 350);
+            this.btn_singlePlayer_returnToMain.Location = new System.Drawing.Point(614, 487);
             this.btn_singlePlayer_returnToMain.Name = "btn_singlePlayer_returnToMain";
             this.btn_singlePlayer_returnToMain.Size = new System.Drawing.Size(174, 41);
             this.btn_singlePlayer_returnToMain.TabIndex = 6;
@@ -65,18 +68,40 @@
             this.btn_singlePlayer_returnToMain.UseVisualStyleBackColor = true;
             this.btn_singlePlayer_returnToMain.Click += new System.EventHandler(this.btn_singlePlayer_returnToMain_Click);
             // 
+            // panel_singlePlayer_top
+            // 
+            this.panel_singlePlayer_top.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.panel_singlePlayer_top.Controls.Add(this.lbl_singlePlayer_title);
+            this.panel_singlePlayer_top.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel_singlePlayer_top.Location = new System.Drawing.Point(0, 0);
+            this.panel_singlePlayer_top.Name = "panel_singlePlayer_top";
+            this.panel_singlePlayer_top.Size = new System.Drawing.Size(800, 52);
+            this.panel_singlePlayer_top.TabIndex = 7;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(121)))), ((int)(((byte)(233)))));
+            this.panel1.Location = new System.Drawing.Point(74, 95);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(654, 362);
+            this.panel1.TabIndex = 8;
+            // 
             // SinglePlayer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 582);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel_singlePlayer_top);
             this.Controls.Add(this.btn_singlePlayer_returnToMain);
             this.Controls.Add(this.btn_singlePlayer_exit);
-            this.Controls.Add(this.lbl_singlePlayer_title);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "SinglePlayer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SinglePlayer";
+            this.panel_singlePlayer_top.ResumeLayout(false);
+            this.panel_singlePlayer_top.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -85,5 +110,7 @@
         private System.Windows.Forms.Label lbl_singlePlayer_title;
         private System.Windows.Forms.Button btn_singlePlayer_exit;
         private System.Windows.Forms.Button btn_singlePlayer_returnToMain;
+        private System.Windows.Forms.Panel panel_singlePlayer_top;
+        private System.Windows.Forms.Panel panel1;
     }
 }
--- ConnectFour_Group7/Welcome.Designer.cs
+++ ConnectFour_Group7/Welcome.Designer.cs
@@ -28,24 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel_welcome_top = new System.Windows.Forms.Panel();
             this.lbl_welcome_title = new System.Windows.Forms.Label();
             this.btn_welcome_singlePlayer = new System.Windows.Forms.Button();
             this.btn_welcome_versus = new System.Windows.Forms.Button();
             this.btn_welcome_statistics = new System.Windows.Forms.Button();
             this.btn_welcome_exit = new System.Windows.Forms.Button();
-            this.panel1.SuspendLayout();
+            this.panel_welcome_top.SuspendLayout();
             this.SuspendLayout();
             // 
-            // panel1
+            // panel_welcome_top
             // 
-            this.panel1.BackColor = System.Drawing.SystemColors.HotTrack;
-            this.panel1.Controls.Add(this.lbl_welcome_title);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(473, 52);
-            this.panel1.TabIndex = 0;
+            this.panel_welcome_top.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.panel_welcome_top.Controls.Add(this.lbl_welcome_title);
+            this.panel_welcome_top.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel_welcome_top.Location = new System.Drawing.Point(0, 0);
+            this.panel_welcome_top.Name = "panel_welcome_top";
+            this.panel_welcome_top.Size = new System.Drawing.Size(473, 52);
+            this.panel_welcome_top.TabIndex = 0;
             // 
             // lbl_welcome_title
             // 
@@ -53,9 +53,9 @@
             this.lbl_welcome_title.Font = new System.Drawing.Font("Comic Sans MS", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbl_welcome_title.Location = new System.Drawing.Point(3, 0);
             this.lbl_welcome_title.Name = "lbl_welcome_title";
-            this.lbl_welcome_title.Size = new System.Drawing.Size(221, 50);
+            this.lbl_welcome_title.Size = new System.Drawing.Size(243, 50);
             this.lbl_welcome_title.TabIndex = 2;
-            this.lbl_welcome_title.Text = "Connect Fo\'";
+            this.lbl_welcome_title.Text = "Connect Fore";
             // 
             // btn_welcome_singlePlayer
             // 
@@ -111,20 +111,20 @@
             this.Controls.Add(this.btn_welcome_statistics);
             this.Controls.Add(this.btn_welcome_versus);
             this.Controls.Add(this.btn_welcome_singlePlayer);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel_welcome_top);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Welcome";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
+            this.panel_welcome_top.ResumeLayout(false);
+            this.panel_welcome_top.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel_welcome_top;
         private System.Windows.Forms.Button btn_welcome_singlePlayer;
         private System.Windows.Forms.Label lbl_welcome_title;
         private System.Windows.Forms.Button btn_welcome_versus;
