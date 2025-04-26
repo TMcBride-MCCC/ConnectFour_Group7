0dfd229b2c760b4a53610b752ad6916023d78cc2
Tyler McBride <tmcbride2@my.monroeccc.edu>
2025-04-26 17:30:20 -0400

    Update Cell.cs
    -Cell class was deleted, re-pushing.

--- ConnectFour_Group7/Cell.cs
+++ ConnectFour_Group7/Cell.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,63 @@ namespace ConnectFour_Group7
 {
     internal class Cell
     {
+        private int row;
+        private int col;
+        private bool isEmpty = true;
+        private string playerThatFilled = null;
+
+        public Cell(int r, int c)
+        {
+            row = r;
+            col = c;
+        }
+
+        //================================================
+        //                      SETTERS
+        //================================================
+
+        public void setRow(int r)
+        {
+            row = r;
+        }
+
+        public void setCol(int c)
+        {
+            col = c;
+        }
+
+        public void setIsEmpty(bool e)
+        {
+            isEmpty = e;
+        }
+
+        public void setPlayerThatFilled(string p)
+        {
+            playerThatFilled = p;
+        }
+
+        //================================================
+        //                      GETTERS
+        //================================================
+
+        public int getRow()
+        {
+            return row;
+        }
+
+        public int getCol()
+        {
+            return col;
+        }
+
+        public bool isEmptyCell()
+        {
+            return isEmpty;
+        }
+
+        public string getPlayerThatFilled()
+        {
+            return playerThatFilled;
+        }
     }
 }
