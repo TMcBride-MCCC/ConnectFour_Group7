d42600b6181cfd70dca5425efb4207a245e39bfa
Tyler McBride <tmcbride2@my.monroeccc.edu>
2025-05-03 19:37:18 -0400

    Update Stats.Designer.cs
    Stats GUI updated

--- ConnectFour_Group7/Stats.Designer.cs
+++ ConnectFour_Group7/Stats.Designer.cs
@@ -28,25 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lbl_stats_title = new System.Windows.Forms.Label();
             this.btn_stats_exit = new System.Windows.Forms.Button();
             this.btn_stats_returnToMain = new System.Windows.Forms.Button();
+            this.panel_Stats_top = new System.Windows.Forms.Panel();
+            this.lbl_singlePlayer_title = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.panel_Stats_top.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // lbl_stats_title
-            // 
-            this.lbl_stats_title.AutoSize = true;
-            this.lbl_stats_title.Font = new System.Drawing.Font("Comic Sans MS", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_stats_title.Location = new System.Drawing.Point(333, 9);
-            this.lbl_stats_title.Name = "lbl_stats_title";
-            this.lbl_stats_title.Size = new System.Drawing.Size(114, 50);
-            this.lbl_stats_title.TabIndex = 4;
-            this.lbl_stats_title.Text = "Stats";
             // 
             // btn_stats_exit
             // 
             this.btn_stats_exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_stats_exit.Location = new System.Drawing.Point(614, 397);
+            this.btn_stats_exit.Location = new System.Drawing.Point(139, 608);
             this.btn_stats_exit.Name = "btn_stats_exit";
             this.btn_stats_exit.Size = new System.Drawing.Size(174, 41);
             this.btn_stats_exit.TabIndex = 5;
@@ -57,7 +64,7 @@
             // btn_stats_returnToMain
             // 
             this.btn_stats_returnToMain.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_stats_returnToMain.Location = new System.Drawing.Point(614, 350);
+            this.btn_stats_returnToMain.Location = new System.Drawing.Point(139, 561);
             this.btn_stats_returnToMain.Name = "btn_stats_returnToMain";
             this.btn_stats_returnToMain.Size = new System.Drawing.Size(174, 41);
             this.btn_stats_returnToMain.TabIndex = 8;
@@ -65,25 +72,229 @@
             this.btn_stats_returnToMain.UseVisualStyleBackColor = true;
             this.btn_stats_returnToMain.Click += new System.EventHandler(this.btn_stats_returnToMain_Click);
             // 
+            // panel_Stats_top
+            // 
+            this.panel_Stats_top.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.panel_Stats_top.Controls.Add(this.lbl_singlePlayer_title);
+            this.panel_Stats_top.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel_Stats_top.Location = new System.Drawing.Point(0, 0);
+            this.panel_Stats_top.Name = "panel_Stats_top";
+            this.panel_Stats_top.Size = new System.Drawing.Size(473, 52);
+            this.panel_Stats_top.TabIndex = 9;
+            // 
+            // lbl_singlePlayer_title
+            // 
+            this.lbl_singlePlayer_title.AutoSize = true;
+            this.lbl_singlePlayer_title.Font = new System.Drawing.Font("Comic Sans MS", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_singlePlayer_title.Location = new System.Drawing.Point(3, 0);
+            this.lbl_singlePlayer_title.Name = "lbl_singlePlayer_title";
+            this.lbl_singlePlayer_title.Size = new System.Drawing.Size(114, 50);
+            this.lbl_singlePlayer_title.TabIndex = 3;
+            this.lbl_singlePlayer_title.Text = "Stats";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(183, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 33);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Player 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(155, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 30);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Wins:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(148, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 30);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Loses:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Comic Sans MS", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(162, 148);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 30);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Ties:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Comic Sans MS", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(141, 178);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 30);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Win %:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Comic Sans MS", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(141, 350);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(85, 30);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "Win %:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Comic Sans MS", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(162, 320);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(64, 30);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Ties:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Comic Sans MS", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(148, 290);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(78, 30);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Loses:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Comic Sans MS", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(155, 260);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(71, 30);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Wins:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(183, 227);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(106, 33);
+            this.label10.TabIndex = 15;
+            this.label10.Text = "Player 2";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Comic Sans MS", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(141, 516);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(85, 30);
+            this.label11.TabIndex = 24;
+            this.label11.Text = "Win %:";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Comic Sans MS", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(162, 486);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(64, 30);
+            this.label12.TabIndex = 23;
+            this.label12.Text = "Ties:";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Comic Sans MS", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(148, 456);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(78, 30);
+            this.label13.TabIndex = 22;
+            this.label13.Text = "Loses:";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Comic Sans MS", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(155, 426);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(71, 30);
+            this.label14.TabIndex = 21;
+            this.label14.Text = "Wins:";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.Location = new System.Drawing.Point(218, 393);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(45, 33);
+            this.label15.TabIndex = 20;
+            this.label15.Text = "AI";
+            // 
             // Stats
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(473, 672);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel_Stats_top);
             this.Controls.Add(this.btn_stats_returnToMain);
             this.Controls.Add(this.btn_stats_exit);
-            this.Controls.Add(this.lbl_stats_title);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Stats";
             this.Text = "Stats";
+            this.panel_Stats_top.ResumeLayout(false);
+            this.panel_Stats_top.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label lbl_stats_title;
         private System.Windows.Forms.Button btn_stats_exit;
         private System.Windows.Forms.Button btn_stats_returnToMain;
+        private System.Windows.Forms.Panel panel_Stats_top;
+        private System.Windows.Forms.Label lbl_singlePlayer_title;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
     }
 }
